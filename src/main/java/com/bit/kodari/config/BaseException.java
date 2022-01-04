f906994d87e66e1e0c229593eb2f558278fd8f42
forceTlight <53989167+forceTlight@users.noreply.github.com>
2022-01-04 17:38:57 +0900

    INIT : Spring JDBC Template generated

---
+++ src/main/java/com/bit/kodari/config/BaseException.java
@@ -0,0 +1,12 @@
+package com.bit.kodari.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class BaseException extends Exception {
+    private BaseResponseStatus status;  //BaseResoinseStatus 객체에 매핑
+}
---
+++ src/main/java/com/bit/kodari/repository/user/UserRepository.java
@@ -0,0 +1,28 @@
+package com.bit.kodari.repository.user;
+
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+public class UserRepository {
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    UserSql userSql;
+    public UserRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+    // User 레코드 추가
+    public User.RegisterRes insert(User.RegisterReq user) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        SqlParameterSource parameterSource = new MapSqlParameterSource("name", user.getName())
+                .addValue("nickName", user.getNickName())
+                .addValue("profileImgUrl", user.getProfileImgUrl())
+                .addValue("email", user.getEmail())
+                .addValue("password", user.getPassword())
+                .addValue("phoneNumber", user.getPhoneNumber())
+                .addValue("deleteYN", "N");
+        int affectedRows = namedParameterJdbcTemplate.update(userSql.INSERT, parameterSource, keyHolder);
+        return User.RegisterRes.builder().name(user.getName()).build();
+    }
+}
