3d94f3d7e2e09dd140ec5f8424f6193bc47e5681
forceTlight <gamebox27@naver.com>
2022-01-14 22:46:22 +0900

    gitignore 적용

--- src/main/java/com/bit/kodari/config/secret/Secret.java
+++
@@ -1,8 +0,0 @@
-package com.bit.kodari.config.secret;
-
-// TODO: 해당 KEY 값들을 꼭 바꿔서 사용해주세요!
-// TODO: .gitignore에 추가하는거 앚지 마세요!
-public class Secret {
-    public static String JWT_SECRET_KEY = "UwKYk0dar2gkW7g-*k.ap9kje-wxBHb9wdXoBT4vnt4P3sJWt-Nu";
-    public static String USER_INFO_PASSWORD_KEY = "o9pqb2tTeam-F8_.PEzEiw!L9F6.AYj9jcfVJ*_i.ifXYnyE68kix@Q2dL6rw*bV-rpdZYwcqZG-jPF-fw3CiJyKsfZ778ks-*jnZn";
-}
