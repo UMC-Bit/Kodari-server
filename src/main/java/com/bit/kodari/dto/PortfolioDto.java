4da74cd4a55f004e97f6ced176fd68d74bd6c51e
Juyeon <anm0912@naver.com>
2022-01-17 13:26:08 +0900

    Portfolio Dto

--- src/main/java/com/bit/kodari/dto/PortfolioDto.java
+++ src/main/java/com/bit/kodari/dto/PortfolioDto.java
@@ -1,4 +1,59 @@
 package com.bit.kodari.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 public class PortfolioDto {
+    //Portfolio 기본 정보
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Portfolio {
+        private int portIdx;
+        private int userIdx;
+        private int accountIdx;
+        private String status;
+    }
+
+    //포트폴리오 생성 Request Dto
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class PostPortfolioReq{
+        private int userIdx;
+        private int accountIdx;
+    }
+
+    //포트폴리오 생성 Response Dto
+    @Data
+    @Builder // 빌더 클래스 자동 생성
+    public static class PostPortfolioRes{
+        private int portIdx;
+        private int userIdx;
+        private int accountIdx;
+    }
+
+    //포트폴리오 삭제 Request Dto
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class PatchPortfolioDelReq{
+        private int portIdx;
+        //    private String jwt;
+    }
+
+    //포트폴리오 삭제 Response Dto
+    @Data
+    @Builder // 빌더 클래스 자동 생성
+    public static class PatchPortfolioDelRes{
+        private int portIdx;
+        private int userIdx;
+        private int accountIdx;
+        private String status;
+        //    private String jwt;
+    }
+
+
 }
