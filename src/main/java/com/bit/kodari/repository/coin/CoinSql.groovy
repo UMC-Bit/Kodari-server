01e3a61e5465ce03e699b36c2770b1ded3181945
chaeanna <annabcd99@gachon.ac.kr>
2022-03-20 23:39:35 +0900

    anna-coin coin 조회 중복 제거

--- src/main/java/com/bit/kodari/repository/coin/CoinSql.groovy
+++ src/main/java/com/bit/kodari/repository/coin/CoinSql.groovy
@@ -6,12 +6,14 @@ class CoinSql {
          SELECT coinIdx, coinName, symbol, coinImg
          FROM Coin
          WHERE status = 'active'
+         GROUP BY coinName
          """
 
     //토론장 코인이름 조회
     public static final String LIST_COIN_NAME = """
-         SELECT coinIdx, coinName, symbol, coinImg
+         SELECT  coinIdx, coinName, symbol, coinImg
          FROM Coin
          WHERE status = 'active' and coinName = :coinName
+         GROUP BY coinName
          """
 }
