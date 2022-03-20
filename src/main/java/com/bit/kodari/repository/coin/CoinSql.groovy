6b3e41965f461ca88f670dd86cff977d0a920f20
chaeanna <annabcd99@gachon.ac.kr>
2022-03-21 00:03:52 +0900

    anna-coin marketIdx별 코인 조회 api 구현

--- src/main/java/com/bit/kodari/repository/coin/CoinSql.groovy
+++ src/main/java/com/bit/kodari/repository/coin/CoinSql.groovy
@@ -11,9 +11,25 @@ class CoinSql {
 
     //토론장 코인이름 조회
     public static final String LIST_COIN_NAME = """
-         SELECT  coinIdx, coinName, symbol, coinImg
+         SELECT coinIdx, coinName, symbol, coinImg
          FROM Coin
          WHERE status = 'active' and coinName = :coinName
          GROUP BY coinName
          """
+
+
+    //MarketIdx별 코인 조회
+    public static final String LIST_MARKET_COIN = """
+         SELECT coinIdx, coinName, symbol, coinImg
+         FROM Coin
+         WHERE status = 'active' and marketIdx = :marketIdx
+         """
+
+    //MarketIdx별 코인 이름 조회
+    public static final String LIST_MARKET_COIN_NAME = """
+         SELECT coinIdx, coinName, symbol, coinImg
+         FROM Coin
+         WHERE status = 'active' and marketIdx = :marketIdx and coinName = :coinName
+         """
+
 }
