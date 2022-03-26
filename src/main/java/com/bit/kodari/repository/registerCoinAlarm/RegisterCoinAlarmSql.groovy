34eb9033217f170782844f8db7cad03c32a6dd31
chaeanna <annabcd99@gachon.ac.kr>
2022-03-26 21:11:00 +0900

    anna-RegisterCoinAlarm 구조 1차

--- src/main/java/com/bit/kodari/repository/registerCoinAlarm/RegisterCoinAlarmSql.groovy
+++ src/main/java/com/bit/kodari/repository/registerCoinAlarm/RegisterCoinAlarmSql.groovy
@@ -1,4 +1,24 @@
 package com.bit.kodari.repository.registerCoinAlarm
 
+import com.bit.kodari.repository.post.PostRepository
+import com.bit.kodari.utils.JwtService
+import lombok.extern.slf4j.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+
+@Slf4j
+@Service
 class RegisterCoinAlarmSql {
+    @Autowired
+    RegisterCoinAlarmRepository registerCoinAlarmRepository;
+    //private final JwtService jwtService; // JWT부분은 7주차에 다루므로 모르셔도 됩니다!
+    @Autowired
+    JwtService jwtService;
+
+    public RegisterCoinAlarmService(RegisterCoinAlarmRepository registerCoinAlarmRepository, JwtService jwtService) {
+        this.registerCoinAlarmRepository = registerCoinAlarmRepository;
+        this.jwtService = jwtService; // JWT부분은 7주차에 다루므로 모르셔도 됩니다!
+    }
+
 }
--- src/main/java/com/bit/kodari/service/RegisterCoinAlarmService.java
+++ src/main/java/com/bit/kodari/service/RegisterCoinAlarmService.java
@@ -1,4 +1,24 @@
 package com.bit.kodari.service;
 
+
+import com.bit.kodari.repository.registerCoinAlarm.RegisterCoinAlarmRepository;
+import com.bit.kodari.utils.JwtService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
 public class RegisterCoinAlarmService {
+    @Autowired
+    RegisterCoinAlarmRepository registerCoinAlarmRepository;
+    //private final JwtService jwtService; // JWT부분은 7주차에 다루므로 모르셔도 됩니다!
+    @Autowired
+    JwtService jwtService;
+
+    public RegisterCoinAlarmService(RegisterCoinAlarmRepository registerCoinAlarmRepository, JwtService jwtService) {
+        this.registerCoinAlarmRepository = registerCoinAlarmRepository;
+        this.jwtService = jwtService; // JWT부분은 7주차에 다루므로 모르셔도 됩니다!
+    }
+
 }
